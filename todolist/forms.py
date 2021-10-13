b706f98fcf639901541016dce38a1c5e78a0f838
Rohith TK <rohith.t@qburst.com>
2021-10-13 19:26:11 +0530

    Added form to capture data and implemented button functions

---
+++ todolist/forms.py
@@ -0,0 +1,8 @@
+from django import forms
+
+
+class TodoListForm(forms.Form):
+    text = forms.CharField(max_length=45,
+                           widget=forms.TextInput(
+                               attrs={'class': 'form-control', 'placeholder': 'Enter to do e.g. Grocery shopping',
+                                      'aria-label': 'Todo', 'aria-describeby': 'add-btn'}))
